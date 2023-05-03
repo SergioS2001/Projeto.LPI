44679797d74b7d8e7519c1cfe07e3679483094e5
SergioS2001 <106494057+SergioSousa39836@users.noreply.github.com>
2023-05-03 22:21:36 +0100

    ajuste de filament

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">    
 <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -7,8 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                                    </a>
                 </div>
 
                 <!-- Navigation Links -->
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,16 +1,16 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Update Password') }}
+    <h2 class="text-lg font-medium text-gray-900">
+                    {{ __('Update Password') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        <p class="mt-1 text-sm text-gray-600">
+                        {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
 
         @if(is_null(auth()->user()->password))
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __("You created your account using Google authentication. You do not need to provide a current password to update your password.") }}
+        <p class="mt-1 text-sm text-gray-600">
+                            {{ __("You created your account using Google authentication. You do not need to provide a current password to update your password.") }}
             </p>
         @endif
     </header>
@@ -46,8 +46,8 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                    class="text-sm text-gray-600"
+                                    >{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
