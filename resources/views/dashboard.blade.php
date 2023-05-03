764c9e2968f11a410c074590aac2c618bfa7a29f
SergioS2001 <106494057+SergioSousa39836@users.noreply.github.com>
2023-05-03 22:52:52 +0100

    homescreen test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-black-200 leading-tight">
         <div>Bem vindo, {{ Auth::user()->name }}!</div>
         </h2>
     </x-slot>
 
     <style>
         .carousel{
-        /*    max-width: 70%; */
-        
-            width: 1200px;
+          align-self: center;
+          height: 700px;
+          width: 100%;
            
         }
     </style>
-
+      
    
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -24,15 +24,26 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://he.ufp.pt/assets/imgs/web-share.png" class="carousel"  alt="First slide"  >
+      <img  src="https://he.ufp.pt/assets/imgs/web-share.png" class="carousel"  alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://he.ufp.pt/assets/imgs/sobre.jpg" class="carousel" alt="Second slide">
+      <img  src="https://he.ufp.pt/assets/imgs/sobre.jpg" class="carousel" alt="Second slide" >
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://he.ufp.pt/assets/imgs/about/valores/768x420_valores.jpg" class="carousel"  alt="Third slide">
+      <img  src="https://he.ufp.pt/assets/imgs/about/valores/768x420_valores.jpg" class="carousel"  alt="Third slide" >
+    </div>
+  <div class="carousel-item">
+      <img  src="https://www.ufp.pt/app/uploads/2020/03/pasop01.jpg" class="carousel"  alt="Forth slide" >
+    </div>
+    <div class="carousel-item">
+      <img src="https://he.ufp.pt/storage/images/services/7_cirurgia-geral_1536056441.jpg" class="carousel"  alt="Fifth slide" >
+    </div>
+    <div class="carousel-item">
+      <img src="https://he.ufp.pt/storage/images/posts/pt/post_271/271_bem-vindo_62a0c081599d8.jpg" class="carousel"  alt="Fifth slide">
     </div>
   </div>
+  </div>
+  
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -46,7 +57,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-light dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-black">
                     //Pequena apresentação programa Estágios/EC
                     <br>
