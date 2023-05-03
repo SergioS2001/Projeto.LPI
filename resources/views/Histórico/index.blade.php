c387ac79e49fef274fd3ed030ca241e34994565e
SergioS2001 <106494057+SergioSousa39836@users.noreply.github.com>
2023-05-03 22:09:43 +0100

    ajuste breeze

--- resources/views/Histórico/index.blade.php
+++ resources/views/Histórico/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         {{ __('Histórico') }}
         </h2>
     </x-slot>
@@ -12,8 +12,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                 <h1> //Table shows agendamentos+estágios for the auth() user</h1>
                 <br>
                 @include('table')
